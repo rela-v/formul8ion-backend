d9491d560f8acb0daf638ff1630a133b2aeff96d
rela <rela.mngrbot@gmail.com>
2024-04-24 11:37:18 -0400

    fix submitForm

--- api/submitForm.js
+++ api/submitForm.js
@@ -4,39 +4,41 @@ require('dotenv').config(); // Load environment variables from .env file
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const mongoDB = process.env.MONGODB_URI;
-{
+
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
+
+  // Define the schema for the form data
+  const formSchema = new mongoose.Schema({
+      id: String,
+      authors: [{
+          name: String,
+          affiliation: String
+      }],
+      figures: [{
+          caption: String,
+          uri: String
+      }],
+      abstract: String,
+      sections: [{
+          title: String,
+          content: String,
+          subsections: [{
+              title: String,
+              content: String,
+              subsubsections: [{
+                  title: String,
+                  content: String
+              }]
+          }]
+      }],
+      references: [{
+          citation: String
+      }]
+  });
+
+  // Export the model
+  module.exports = mongoose.model("Form", formSchema);
 }
 
-// Define the schema for the form data
-const formSchema = new mongoose.Schema({
-    id: String,
-    authors: [{
-        name: String,
-        affiliation: String
-    }],
-    figures: [{
-        caption: String,
-        uri: String
-    }],
-    abstract: String,
-    sections: [{
-        title: String,
-        content: String,
-        subsections: [{
-            title: String,
-            content: String,
-            subsubsections: [{
-                title: String,
-                content: String
-            }]
-        }]
-    }],
-    references: [{
-        citation: String
-    }]
-});
-
-module.exports = mongoose.model("Form", formSchema);
