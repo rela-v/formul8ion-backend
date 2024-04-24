c7fd39d3a7891bc30135d91454a67424962b5c35
rela <rela.mngrbot@gmail.com>
2024-04-23 21:34:11 -0400

    add refactoring

--- api/submitForm.js
+++ api/submitForm.js
@@ -1,16 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config(); // Load environment variables from .env file
 
-// Connect to MongoDB Atlas
-const MONGODB_URI = process.env.MONGODB_URI;
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB Atlas');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB Atlas:', error);
-  });
-
 // Define the schema for the form data
 const formSchema = new mongoose.Schema({
     id: String,
@@ -43,22 +33,34 @@ const formSchema = new mongoose.Schema({
 // Define the model based on the schema
 const FormModel = mongoose.model('Form', formSchema);
 
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGODB_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true // Added for better performance
+})
+.then(() => {
+    console.log('Connected to MongoDB Atlas');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+});
+
 // Define the handler for the submit-form function
 module.exports = async (req, res) => {
     try {
-      // Create a new form document using the FormModel
-      const formData = req.body;
-      const form = new FormModel(formData);
-  
-      // Save the form document to the database
-      await form.save();
-  
-      // Send a response indicating success
-      res.json({ message: 'Form submitted successfully.' });
+        // Create a new form document using the FormModel
+        const formData = req.body;
+        const form = new FormModel(formData);
+
+        // Save the form document to the database
+        await form.save();
+
+        // Send a response indicating success
+        res.json({ message: 'Form submitted successfully.' });
     } catch (error) {
-      // If an error occurs, send a response indicating failure
-      console.error('Error submitting form:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+        // If an error occurs, send a response indicating failure
+        console.error('Error submitting form:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 };
-
